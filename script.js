7239b0596f70ab336c80269114d55803b7a435eb
Balazs Juhasz <juhaszbal@gmail.com>
2021-01-28 16:01:50 +0100

    finish game scoring functionality

--- script.js
+++ script.js
@@ -40,6 +40,40 @@ let finalTimeDisplay = '0.0s';
 // Scroll
 let valueY = 0;
 
+// Reset Game
+const playAgain = () => {
+  gamePage.addEventListener('click', startTimer);
+  scorePage.hidden = true;
+  splashPage.hidden = false;
+  equationsArray = [];
+  playerGuessArray = [];
+  valueY = 0;
+  playAgainBtn.hidden = true;
+};
+
+// Show Score Page
+const showScorePage = () => {
+  // Show Play Again button after 1 second
+  setTimeout(() => {
+    playAgainBtn.hidden = false;
+  }, 1000);
+  gamePage.hidden = true;
+  scorePage.hidden = false;
+};
+
+// Format & Display Time in DOM
+const scoresToDOM = () => {
+  finalTimeDisplay = finalTime.toFixed(1);
+  baseTime = timePlayed.toFixed(1);
+  penaltyTime = penaltyTime.toFixed(1);
+  baseTimeEl.textContent = `Base Time: ${baseTime}s`;
+  penaltyTimeEl.textContent = `Penalty: +${penaltyTime}s`;
+  finalTimeEl.textContent = `${finalTimeDisplay}s`;
+  // Scroll to Top, go to Score Page
+  itemContainer.scrollTo({ top: 0, behavior: 'instant' });
+  showScorePage();
+};
+
 // Stop Timer, Process Results, go to Score Page
 const checkTime = () => {
   console.log(timePlayed);
@@ -64,6 +98,7 @@ const checkTime = () => {
       'final:',
       finalTime
     );
+    scoresToDOM();
   }
 };
 
